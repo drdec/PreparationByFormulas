2daedab40230e1fa6806f069a113908ba39501d4
Dr. Deckto <fynjy138@gmail.com>
2023-02-23 02:51:00 +0300

    добавить считывание картинок

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
 
 namespace WpfApp1
 {
@@ -7,9 +10,30 @@ namespace WpfApp1
     /// </summary>
     public partial class MainWindow : Window
     {
+        private static int _cz = 1;
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            BitmapImage image = new BitmapImage();
+
+            var fileName = _cz + ".png";
+
+            using (FileStream stream = File.OpenRead(@"C:\Project\PreparationByFormulas\PreparationByFormulas\Image\" + fileName))
+            {
+                image.BeginInit();
+                image.StreamSource = stream;
+                image.CacheOption = BitmapCacheOption.OnLoad;
+                image.EndInit();
+            }
+
+            _cz++;
+
+            img.Source = image;
+        }
+
     }
 }
